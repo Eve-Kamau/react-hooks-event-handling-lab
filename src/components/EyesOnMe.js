59901cc30f4553699fa9cfb5da73ef64a864da8e
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-02-13 21:58:45 +0300

    Completed Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
+import React from 'react';
+
+function EyesOnMe() {
+    function handleClick(event) {
+    console.log("Good!");
+    }
+
+    function handleBlur(event) {
+    console.log("Hey! Eyes on me!");
+    }
+
+   return (
+    <div>
+    <button 
+    onFocus={handleClick} 
+    onBlur={handleBlur}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe;
+
 // Code EyesOnMe Component Here
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,20 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleChange(event) {
+    console.log("Entering password...");
+    }
+  return (
+    <div>
+    <input type="password" 
+    name="keypad" 
+    placeholder="Pls Enter Password" 
+    onChange={handleChange} />
+    </div>
+  )
 }
 
 export default Keypad;
+
+
+//Code Keypad Component Here
